4b786823be13fdc154a10f26b59dcbebf2ba9782
David Dal Busco <david.dalbusco@dfinity.org>
2025-12-02 18:11:29 +0100

    feat: simplify rexport

--- packages/cmc/src/index.ts
+++ packages/cmc/src/index.ts
@@ -2,19 +2,11 @@
  * @module api/cmc
  */
 
-import type {CmcDid} from "@icp-sdk/canisters/cmc";
+import type { CmcDid } from "@icp-sdk/canisters/cmc";
 
 export type Cycles = CmcDid.Cycles;
 export type NotifyCreateCanisterArg = CmcDid.NotifyCreateCanisterArg;
 export type NotifyTopUpArg = CmcDid.NotifyTopUpArg;
 export type SubnetTypesToSubnetsResponse = CmcDid.SubnetTypesToSubnetsResponse;
 
-export { CMCCanister } from "@icp-sdk/canisters/cmc";
-export {
-  RefundedError,
-  InvalidaTransactionError,
-  CMCError,
-  ProcessingError,
-  TransactionTooOldError,
-  throwNotifyError
-} from "@icp-sdk/canisters/cmc";
+export * from "@icp-sdk/canisters/cmc";
