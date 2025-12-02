e85d47c298bb7d1342300484cbbbe3c6f35401c3
David Dal Busco <david.dalbusco@dfinity.org>
2025-12-02 14:50:43 +0100

    feat(canisters): copy cmc source code

---
+++ packages/canisters/src/cmc/cmc.options.ts
@@ -0,0 +1,10 @@
+import type { CanisterOptions } from "@dfinity/utils";
+import type { Principal } from "@icp-sdk/core/principal";
+import type { _SERVICE as CanisterService } from "../declarations/cmc/cmc";
+
+export interface CMCCanisterOptions extends Omit<
+  CanisterOptions<CanisterService>,
+  "canisterId"
+> {
+  canisterId: Principal;
+}
