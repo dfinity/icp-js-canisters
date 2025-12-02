d7431a4c4782ef39ef0d244f6b6b7f8e35c563e3
David Dal Busco <david.dalbusco@dfinity.org>
2025-12-02 15:47:33 +0100

    chore: fmt

--- packages/canisters/src/cmc/cmc.options.ts
+++ packages/canisters/src/cmc/cmc.options.ts
@@ -2,7 +2,9 @@ import type { CanisterOptions } from "@dfinity/utils";
 import type { Principal } from "@icp-sdk/core/principal";
 import type { CmcActor } from "../declarations";
 
-export interface CMCCanisterOptions
-  extends Omit<CanisterOptions<CmcActor>, "canisterId"> {
+export interface CMCCanisterOptions extends Omit<
+  CanisterOptions<CmcActor>,
+  "canisterId"
+> {
   canisterId: Principal;
 }
