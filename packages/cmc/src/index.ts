836834c9bf1faf7bfd5a9806ba1f8f33bbaa3fd0
David Dal Busco <david.dalbusco@dfinity.org>
2025-12-02 18:08:43 +0100

    feat(cmc): use icp-sdk/canisters/cmc in dfinity/cmc

--- packages/cmc/src/index.ts
+++ packages/cmc/src/index.ts
@@ -2,11 +2,19 @@
  * @module api/cmc
  */
 
-export type {
-  Cycles,
-  NotifyCreateCanisterArg,
-  NotifyTopUpArg,
-  SubnetTypesToSubnetsResponse,
-} from "./candid/cmc";
-export { CMCCanister } from "./cmc.canister";
-export * from "./cmc.errors";
+import type {CmcDid} from "@icp-sdk/canisters/cmc";
+
+export type Cycles = CmcDid.Cycles;
+export type NotifyCreateCanisterArg = CmcDid.NotifyCreateCanisterArg;
+export type NotifyTopUpArg = CmcDid.NotifyTopUpArg;
+export type SubnetTypesToSubnetsResponse = CmcDid.SubnetTypesToSubnetsResponse;
+
+export { CMCCanister } from "@icp-sdk/canisters/cmc";
+export {
+  RefundedError,
+  InvalidaTransactionError,
+  CMCError,
+  ProcessingError,
+  TransactionTooOldError,
+  throwNotifyError
+} from "@icp-sdk/canisters/cmc";
