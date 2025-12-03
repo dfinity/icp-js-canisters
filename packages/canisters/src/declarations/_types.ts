2f3d3401c40f0c5c29e5796b09ca889e12a832d6
David Dal Busco <david.dalbusco@dfinity.org>
2025-12-03 07:05:26 +0100

    feat(canisters): expose ic-mgmt did namespace and services

--- packages/canisters/src/declarations/_types.ts
+++ packages/canisters/src/declarations/_types.ts
@@ -1,4 +1,5 @@
 import type * as AssetsDid from "./assets/assets_assetstorage";
 import type * as CmcDid from "./cmc/cmc";
+import type * as IcManagementDid from "./ic-management/ic-management";
 
-export type { AssetsDid, CmcDid };
+export type { AssetsDid, CmcDid, IcManagementDid };
