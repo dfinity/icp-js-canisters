3a518b0c460420410044a25d17742c3de4c06d56
David Dal Busco <david.dalbusco@dfinity.org>
2025-12-08 11:01:26 +0100

    feat(canisters): copy ckbtc source code

--- packages/canisters/src/ckbtc/index.ts
+++ packages/canisters/src/ckbtc/index.ts
@@ -1,1 +1,35 @@
-export * from "@dfinity/ckbtc";
+/**
+ * @module api/ckbtc
+ */
+
+export type {
+  block_hash,
+  block_height,
+  get_utxos_response,
+  outpoint,
+  satoshi,
+  utxo,
+} from "../declarations/ckbtc/bitcoin";
+export type {
+  Account,
+  MinterInfo,
+  PendingUtxo,
+  ReimbursedDeposit,
+  ReimbursementRequest,
+  RetrieveBtcOk,
+  RetrieveBtcStatus,
+  RetrieveBtcStatusV2,
+  Utxo,
+  UtxoStatus,
+  Account as WithdrawalAccount,
+} from "../declarations/ckbtc/minter";
+export { BitcoinCanister } from "./bitcoin.canister";
+export * from "./enums/btc.enums";
+export * from "./errors/btc.errors";
+export * from "./errors/minter.errors";
+export { CkBTCMinterCanister } from "./minter.canister";
+export * from "./types/bitcoin.params";
+export * from "./types/btc";
+export * from "./types/minter.params";
+export * from "./types/minter.responses";
+export * from "./utils/btc.utils";
