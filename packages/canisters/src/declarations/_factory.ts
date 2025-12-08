9890abe227cd5f5dcf023c529f3a3c47c7603083
David Dal Busco <david.dalbusco@dfinity.org>
2025-12-08 06:47:07 +0100

    feat(canisters): expose ledger ICP did namespace and services

--- packages/canisters/src/declarations/_factory.ts
+++ packages/canisters/src/declarations/_factory.ts
@@ -3,18 +3,30 @@ import { idlFactory as idlFactoryCertifiedCmc } from "./cmc/cmc.certified.idl";
 import { idlFactory as idlFactoryCmc } from "./cmc/cmc.idl";
 import { idlFactory as idlFactoryCertifiedIcManagement } from "./ic-management/ic-management.certified.idl";
 import { idlFactory as idlFactoryIcManagement } from "./ic-management/ic-management.idl";
+import { idlFactory as idlFactoryCertifiedIcpIndex } from "./ledger-icp/index.certified.idl";
+import { idlFactory as idlFactoryIcpIndex } from "./ledger-icp/index.idl";
+import { idlFactory as idlFactoryCertifiedIcpLedger } from "./ledger-icp/ledger.certified.idl";
+import { idlFactory as idlFactoryIcpLedger } from "./ledger-icp/ledger.idl";
 
 import type { _SERVICE as AssetsService } from "./assets/assets_assetstorage";
 import type { _SERVICE as CmcService } from "./cmc/cmc";
 import type { _SERVICE as IcManagementService } from "./ic-management/ic-management";
+import type { _SERVICE as IcpIndexService } from "./ledger-icp/index";
+import type { _SERVICE as IcpLedgerService } from "./ledger-icp/ledger";
 
 export {
   idlFactoryAssets,
   idlFactoryCertifiedCmc,
   idlFactoryCertifiedIcManagement,
+  idlFactoryCertifiedIcpIndex,
+  idlFactoryCertifiedIcpLedger,
   idlFactoryCmc,
   idlFactoryIcManagement,
+  idlFactoryIcpIndex,
+  idlFactoryIcpLedger,
   type AssetsService,
   type CmcService,
   type IcManagementService,
+  type IcpIndexService,
+  type IcpLedgerService,
 };
--- packages/canisters/src/declarations/_types.ts
+++ packages/canisters/src/declarations/_types.ts
@@ -1,5 +1,7 @@
 import type * as AssetsDid from "./assets/assets_assetstorage";
 import type * as CmcDid from "./cmc/cmc";
 import type * as IcManagementDid from "./ic-management/ic-management";
+import type * as IcpIndexDid from "./ledger-icp/index";
+import type * as IcpLedgerDid from "./ledger-icp/ledger";
 
-export type { AssetsDid, CmcDid, IcManagementDid };
+export type { AssetsDid, CmcDid, IcManagementDid, IcpIndexDid, IcpLedgerDid };
