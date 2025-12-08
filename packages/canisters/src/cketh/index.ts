a44addcdfdc4096a583d8395586fdcbf028670a3
David Dal Busco <david.dalbusco@dfinity.org>
2025-12-08 13:45:14 +0100

    feat(canisters): expose cketh canister options

--- packages/canisters/src/cketh/index.ts
+++ packages/canisters/src/cketh/index.ts
@@ -6,5 +6,6 @@ export type { CkEthMinterDid, CkEthOrchestratorDid } from "../declarations";
 export * from "./errors/minter.errors";
 export { CkEthMinterCanister } from "./minter.canister";
 export { CkEthOrchestratorCanister } from "./orchestrator.canister";
+export type * from "./types/canister.options";
 export type { Eip1559TransactionPriceParams } from "./types/minter.params";
 export * from "./utils/minter.utils";
