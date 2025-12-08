5ab7a6e1da1e54185f29952835f2bacdc27431d9
David Dal Busco <david.dalbusco@dfinity.org>
2025-12-08 11:02:55 +0100

    feat(canisters): copy cketh source code

--- packages/canisters/src/cketh/index.ts
+++ packages/canisters/src/cketh/index.ts
@@ -1,1 +1,26 @@
-export * from "@dfinity/cketh";
+/**
+ * @module api/cketh
+ */
+
+export type {
+  Eip1559TransactionPrice,
+  EthTransaction,
+  MinterInfo,
+  RetrieveErc20Request,
+  RetrieveEthRequest,
+  RetrieveEthStatus,
+  Subaccount,
+  TxFinalizedStatus,
+} from "../declarations/cketh/minter";
+export type {
+  CyclesManagement,
+  Erc20Contract,
+  ManagedCanisterStatus,
+  ManagedCanisters,
+  OrchestratorInfo,
+} from "../declarations/cketh/orchestrator";
+export * from "./errors/minter.errors";
+export { CkETHMinterCanister } from "./minter.canister";
+export { CkETHOrchestratorCanister } from "./orchestrator.canister";
+export type { Eip1559TransactionPriceParams } from "./types/minter.params";
+export * from "./utils/minter.utils";
