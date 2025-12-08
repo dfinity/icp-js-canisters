585252b89a58c9fdf1d08e6256000327395e39ca
David Dal Busco <david.dalbusco@dfinity.org>
2025-12-08 10:58:30 +0100

    feat(canisters): expose ckbtc did namespace and services (#1346)
    # Motivation
    
    We want to use the new "DID namespace" pattern for any package we
    integrate in canisters.
    
    # Changes
    
    - Create namespace and services for ckbtc (follow-up of #1347)

--- packages/canisters/src/declarations/_factory.ts
+++ packages/canisters/src/declarations/_factory.ts
@@ -1,4 +1,8 @@
 import { idlFactory as idlFactoryAssets } from "./assets/assets_assetstorage.idl";
+import { idlFactory as idlFactoryCertifiedCkBtcBitcoin } from "./ckbtc/bitcoin.certified.idl";
+import { idlFactory as idlFactoryCkBtcBitcoin } from "./ckbtc/bitcoin.idl";
+import { idlFactory as idlFactoryCertifiedCkBtcMinter } from "./ckbtc/minter.certified.idl";
+import { idlFactory as idlFactoryCkBtcMinter } from "./ckbtc/minter.idl";
 import { idlFactory as idlFactoryCertifiedCkEthMinter } from "./cketh/minter.certified.idl";
 import { idlFactory as idlFactoryCkEthMinter } from "./cketh/minter.idl";
 import { idlFactory as idlFactoryCertifiedCkEthOrchestrator } from "./cketh/orchestrator.certified.idl";
@@ -9,6 +13,8 @@ import { idlFactory as idlFactoryCertifiedIcManagement } from "./ic-management/i
 import { idlFactory as idlFactoryIcManagement } from "./ic-management/ic-management.idl";
 
 import type { _SERVICE as AssetsService } from "./assets/assets_assetstorage";
+import type { _SERVICE as CkBtcBitcoinService } from "./ckbtc/bitcoin";
+import type { _SERVICE as CkBtcMinterService } from "./ckbtc/minter";
 import type { _SERVICE as CkEthMinterService } from "./cketh/minter";
 import type { _SERVICE as CkEthOrchestratorService } from "./cketh/orchestrator";
 import type { _SERVICE as CmcService } from "./cmc/cmc";
@@ -16,15 +22,21 @@ import type { _SERVICE as IcManagementService } from "./ic-management/ic-managem
 
 export {
   idlFactoryAssets,
+  idlFactoryCertifiedCkBtcBitcoin,
+  idlFactoryCertifiedCkBtcMinter,
   idlFactoryCertifiedCkEthMinter,
   idlFactoryCertifiedCkEthOrchestrator,
   idlFactoryCertifiedCmc,
   idlFactoryCertifiedIcManagement,
+  idlFactoryCkBtcBitcoin,
+  idlFactoryCkBtcMinter,
   idlFactoryCkEthMinter,
   idlFactoryCkEthOrchestrator,
   idlFactoryCmc,
   idlFactoryIcManagement,
   type AssetsService,
+  type CkBtcBitcoinService,
+  type CkBtcMinterService,
   type CkEthMinterService,
   type CkEthOrchestratorService,
   type CmcService,
--- packages/canisters/src/declarations/_types.ts
+++ packages/canisters/src/declarations/_types.ts
@@ -1,4 +1,6 @@
 import type * as AssetsDid from "./assets/assets_assetstorage";
+import type * as CkBtcBitcoinDid from "./ckbtc/bitcoin";
+import type * as CkBtcMinterDid from "./ckbtc/minter";
 import type * as CkEthMinterDid from "./cketh/minter";
 import type * as CkEthOrchestratorDid from "./cketh/orchestrator";
 import type * as CmcDid from "./cmc/cmc";
@@ -6,6 +8,8 @@ import type * as IcManagementDid from "./ic-management/ic-management";
 
 export type {
   AssetsDid,
+  CkBtcBitcoinDid,
+  CkBtcMinterDid,
   CkEthMinterDid,
   CkEthOrchestratorDid,
   CmcDid,
