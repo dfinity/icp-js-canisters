1f386eea46d48fc67ee64ea05fb4828f1a9a264f
David Dal Busco <david.dalbusco@dfinity.org>
2025-12-08 06:39:58 +0100

    feat(canisters): expose ckbtc did namespace and services

--- packages/canisters/src/declarations/_factory.ts
+++ packages/canisters/src/declarations/_factory.ts
@@ -1,20 +1,32 @@
 import { idlFactory as idlFactoryAssets } from "./assets/assets_assetstorage.idl";
+import { idlFactory as idlFactoryCertifiedCkBtcBitcoin } from "./ckbtc/bitcoin.certified.idl";
+import { idlFactory as idlFactoryCkBtcBitcoin } from "./ckbtc/bitcoin.idl";
+import { idlFactory as idlFactoryCertifiedCkBtcMinter } from "./ckbtc/minter.certified.idl";
+import { idlFactory as idlFactoryCkBtcMinter } from "./ckbtc/minter.idl";
 import { idlFactory as idlFactoryCertifiedCmc } from "./cmc/cmc.certified.idl";
 import { idlFactory as idlFactoryCmc } from "./cmc/cmc.idl";
 import { idlFactory as idlFactoryCertifiedIcManagement } from "./ic-management/ic-management.certified.idl";
 import { idlFactory as idlFactoryIcManagement } from "./ic-management/ic-management.idl";
 
 import type { _SERVICE as AssetsService } from "./assets/assets_assetstorage";
+import type { _SERVICE as CkBtcBitcoinService } from "./ckbtc/bitcoin";
+import type { _SERVICE as CkBtcMinterService } from "./ckbtc/minter";
 import type { _SERVICE as CmcService } from "./cmc/cmc";
 import type { _SERVICE as IcManagementService } from "./ic-management/ic-management";
 
 export {
   idlFactoryAssets,
+  idlFactoryCertifiedCkBtcBitcoin,
+  idlFactoryCertifiedCkBtcMinter,
   idlFactoryCertifiedCmc,
   idlFactoryCertifiedIcManagement,
+  idlFactoryCkBtcBitcoin,
+  idlFactoryCkBtcMinter,
   idlFactoryCmc,
   idlFactoryIcManagement,
   type AssetsService,
+  type CkBtcBitcoinService,
+  type CkBtcMinterService,
   type CmcService,
   type IcManagementService,
 };
--- packages/canisters/src/declarations/_types.ts
+++ packages/canisters/src/declarations/_types.ts
@@ -1,5 +1,13 @@
 import type * as AssetsDid from "./assets/assets_assetstorage";
+import type * as CkBtcBitcoinDid from "./ckbtc/bitcoin";
+import type * as CkBtcMinterDid from "./ckbtc/minter";
 import type * as CmcDid from "./cmc/cmc";
 import type * as IcManagementDid from "./ic-management/ic-management";
 
-export type { AssetsDid, CmcDid, IcManagementDid };
+export type {
+  AssetsDid,
+  CkBtcBitcoinDid,
+  CkBtcMinterDid,
+  CmcDid,
+  IcManagementDid,
+};
