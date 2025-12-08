bca94bf6d2d4807875c41d00a087071118b566b3
David Dal Busco <david.dalbusco@dfinity.org>
2025-12-08 07:54:43 +0100

    feat(canisters): expose Sns did namespace and services

--- packages/canisters/src/declarations/_factory.ts
+++ packages/canisters/src/declarations/_factory.ts
@@ -3,18 +3,42 @@ import { idlFactory as idlFactoryCertifiedCmc } from "./cmc/cmc.certified.idl";
 import { idlFactory as idlFactoryCmc } from "./cmc/cmc.idl";
 import { idlFactory as idlFactoryCertifiedIcManagement } from "./ic-management/ic-management.certified.idl";
 import { idlFactory as idlFactoryIcManagement } from "./ic-management/ic-management.idl";
+import { idlFactory as idlFactoryCertifiedSnsGovernance } from "./sns/governance.certified.idl";
+import { idlFactory as idlFactorySnsGovernance } from "./sns/governance.idl";
+import { idlFactory as idlFactoryCertifiedSnsGovernanceTest } from "./sns/governance_test.certified.idl";
+import { idlFactory as idlFactorySnsGovernanceTest } from "./sns/governance_test.idl";
+import { idlFactory as idlFactoryCertifiedSnsRoot } from "./sns/root.certified.idl";
+import { idlFactory as idlFactorySnsRoot } from "./sns/root.idl";
+import { idlFactory as idlFactoryCertifiedSnsSwap } from "./sns/swap.certified.idl";
+import { idlFactory as idlFactorySnsSwap } from "./sns/swap.idl";
 
 import type { _SERVICE as AssetsService } from "./assets/assets_assetstorage";
 import type { _SERVICE as CmcService } from "./cmc/cmc";
 import type { _SERVICE as IcManagementService } from "./ic-management/ic-management";
+import type { _SERVICE as SnsGovernanceService } from "./sns/governance";
+import type { _SERVICE as SnsGovernanceTestService } from "./sns/governance_test";
+import type { _SERVICE as SnsRootService } from "./sns/root";
+import type { _SERVICE as SnsSwapService } from "./sns/swap";
 
 export {
   idlFactoryAssets,
   idlFactoryCertifiedCmc,
   idlFactoryCertifiedIcManagement,
+  idlFactoryCertifiedSnsGovernance,
+  idlFactoryCertifiedSnsGovernanceTest,
+  idlFactoryCertifiedSnsRoot,
+  idlFactoryCertifiedSnsSwap,
   idlFactoryCmc,
   idlFactoryIcManagement,
+  idlFactorySnsGovernance,
+  idlFactorySnsGovernanceTest,
+  idlFactorySnsRoot,
+  idlFactorySnsSwap,
   type AssetsService,
   type CmcService,
   type IcManagementService,
+  type SnsGovernanceService,
+  type SnsGovernanceTestService,
+  type SnsRootService,
+  type SnsSwapService,
 };
--- packages/canisters/src/declarations/_types.ts
+++ packages/canisters/src/declarations/_types.ts
@@ -1,5 +1,17 @@
 import type * as AssetsDid from "./assets/assets_assetstorage";
 import type * as CmcDid from "./cmc/cmc";
 import type * as IcManagementDid from "./ic-management/ic-management";
+import type * as SnsGovernanceDid from "./sns/governance";
+import type * as SnsGovernanceTestDid from "./sns/governance_test";
+import type * as SnsRootDid from "./sns/root";
+import type * as SnsSwapDid from "./sns/swap";
 
-export type { AssetsDid, CmcDid, IcManagementDid };
+export type {
+  AssetsDid,
+  CmcDid,
+  IcManagementDid,
+  SnsGovernanceDid,
+  SnsGovernanceTestDid,
+  SnsRootDid,
+  SnsSwapDid,
+};
