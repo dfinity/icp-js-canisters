1d7bddcc2f7fa56a6253123828c14797d7ab5415
David Dal Busco <david.dalbusco@dfinity.org>
2025-12-09 17:03:42 +0100

    feat(ledger-icrc): use @icp-sdk/canisters/ledger/icrc in @dfinity/ledger-icrc

---
+++ packages/ledger-icrc/src/index.spec.ts
@@ -0,0 +1,24 @@
+import {
+  IcrcIndexNgCanister,
+  IcrcIndexCanister,
+  IcrcLedgerCanister,
+  IcrcNftLedgerCanister,
+} from "./index";
+
+describe("@dfinity/ledger-icrc", () => {
+  it("should re-export IcrcIndexNgCanister", () => {
+    expect(IcrcIndexNgCanister).not.toBeUndefined();
+  });
+
+  it("should re-export IcrcIndexCanister", () => {
+    expect(IcrcIndexCanister).not.toBeUndefined();
+  });
+
+  it("should re-export IcrcLedgerCanister", () => {
+    expect(IcrcLedgerCanister).not.toBeUndefined();
+  });
+
+  it("should re-export IcrcNftLedgerCanister", () => {
+    expect(IcrcNftLedgerCanister).not.toBeUndefined();
+  });
+});
