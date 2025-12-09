ea23522558cd2af796ec3bafac9c565315dea1aa
David Dal Busco <david.dalbusco@dfinity.org>
2025-12-09 13:30:13 +0100

    feat(canisters): copy nns source code (#1381)
    # Motivation
    
    We want to embed the source code of nns into canisters.
    
    # Notes
    
    This PR is basically refactoring. No logical nor imports changes.
    
    # Changes
    
    - Copy/paste code from `@dfinity/nns` into package `canisters`
    - Update imports from `./candid` to `./declarations` (no namespace, just
    update path)

---
+++ packages/canisters/src/nns/types/governance.options.ts
@@ -0,0 +1,10 @@
+import type { CanisterOptions } from "@dfinity/utils";
+import type { ActorSubclass } from "@icp-sdk/core/agent";
+import type { _SERVICE as GovernanceService } from "../../declarations/nns/governance";
+
+export interface GovernanceCanisterOptions extends CanisterOptions<GovernanceService> {
+  // Ledger IC App needs requests built with Protobuf.
+  // This flag ensures that the methods use Protobuf.
+  hardwareWallet?: boolean;
+  oldListNeuronsServiceOverride?: ActorSubclass<GovernanceService>;
+}
