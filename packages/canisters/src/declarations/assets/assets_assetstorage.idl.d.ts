e4a36c4a838ff65dc654c1a7ec99a0d5a56e2023
David Dal Busco <david.dalbusco@dfinity.org>
2025-12-10 15:11:27 +0100

    fix(fmt): prettier not run on declarations (#1404)
    # Motivation
    
    Generatid the weekly did updates leads to many differences because
    prettier actually did not run on `declarations` subfolders.
    
    See #1403.
    
    # Changes
    
    - Run prettier on `declarations`
    - Autoformat with CI
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- packages/canisters/src/declarations/assets/assets_assetstorage.idl.d.ts
+++ packages/canisters/src/declarations/assets/assets_assetstorage.idl.d.ts
@@ -1,1 +1,2 @@
-import type { IDL } from "@icp-sdk/core/candid";export const idlFactory: IDL.InterfaceFactory;
+import type { IDL } from "@icp-sdk/core/candid";
+export const idlFactory: IDL.InterfaceFactory;
