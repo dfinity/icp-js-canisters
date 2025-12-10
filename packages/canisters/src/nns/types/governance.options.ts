40dc1c4ad5914bb709b80f802fe3a625bee996e1
David Dal Busco <david.dalbusco@dfinity.org>
2025-12-10 11:19:02 +0100

    feat(canisters): prefix Governance and GenesisToken with Nns

--- packages/canisters/src/nns/types/governance.options.ts
+++ packages/canisters/src/nns/types/governance.options.ts
@@ -2,7 +2,7 @@ import type { CanisterOptions } from "@dfinity/utils";
 import type { ActorSubclass } from "@icp-sdk/core/agent";
 import type { NnsGovernanceService } from "../../declarations";
 
-export interface GovernanceCanisterOptions extends CanisterOptions<NnsGovernanceService> {
+export interface NnsGovernanceCanisterOptions extends CanisterOptions<NnsGovernanceService> {
   // Ledger IC App needs requests built with Protobuf.
   // This flag ensures that the methods use Protobuf.
   hardwareWallet?: boolean;
