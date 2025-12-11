f49df22364e1dda09d3169cf3183d453b8a5521e
ilbertt <luca.bertelli@dfinity.org>
2025-12-11 12:20:40 +0100

    test: fix content type

--- packages/canisters/src/assets/readable/readableFile.spec.ts
+++ packages/canisters/src/assets/readable/readableFile.spec.ts
@@ -6,7 +6,7 @@ describe("ReadableFile", () => {
     const readable = new ReadableFile(file);
 
     expect(readable.fileName).toEqual(file.name);
-    expect(readable.contentType).toEqual("text/plain");
+    expect(readable.contentType).toEqual("");
     expect(readable).toHaveLength(file.size);
 
     await readable.open();
@@ -16,5 +16,16 @@ describe("ReadableFile", () => {
     );
 
     await readable.close();
+  });
+
+  test("ReadableFile from File with mime type", () => {
+    const file = new File(["Hello world!"], "hello.txt", {
+      type: "text/plain",
+    });
+    const readable = new ReadableFile(file);
+
+    expect(readable.fileName).toEqual(file.name);
+    expect(readable.contentType).toEqual("text/plain");
+    expect(readable).toHaveLength(file.size);
   });
 });
