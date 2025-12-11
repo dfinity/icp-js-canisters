59cd0d4f7dd3042995f1b3afe3dfa5eed61a2758
Luca Bertelli <luca.bertelli@dfinity.org>
2025-12-11 14:42:14 +0100

    refactor(assets): kebab case for assets files (#1414)
    # Motivation
    
    Follow this repo's standards.
    
    # Changes
    
    - Renamed files from camelCase to kebab-case

---
+++ packages/canisters/src/assets/readable/readable-path.ts
@@ -0,0 +1,99 @@
+/* eslint-disable local-rules/prefer-object-params */
+
+import { uint8FromBufLike } from "@icp-sdk/core/agent";
+import mime from "mime/lite.js";
+import fs from "../utils/fs-polyfill";
+import path from "../utils/path-polyfill";
+import type { Readable } from "./readable";
+
+export class ReadablePath implements Readable {
+  private readonly _path: string;
+  private readonly _size: number;
+  private _fd?: number;
+
+  protected constructor(path: string, size: number) {
+    this._path = path;
+    this._size = size;
+  }
+
+  public get fileName(): string {
+    return path.basename(this._path);
+  }
+
+  public get contentType(): string {
+    return mime.getType(this.fileName) ?? "application/octet-stream";
+  }
+
+  public get length(): number {
+    return this._size;
+  }
+
+  static create(path: string): Promise<ReadablePath> {
+    return new Promise<ReadablePath>((resolve, reject) => {
+      fs.stat(path, (err, stats) => {
+        if (err) {
+          reject(err);
+          return;
+        }
+        resolve(new ReadablePath(path, stats.size));
+      });
+    });
+  }
+
+  public open(): Promise<void> {
+    return new Promise<void>((resolve, reject) => {
+      if (this._fd !== undefined) {
+        reject("File is already open");
+        return;
+      }
+      fs.open(this._path, (err, fd) => {
+        if (err) {
+          reject(err);
+          return;
+        }
+        this._fd = fd;
+        resolve();
+      });
+    });
+  }
+
+  public close(): Promise<void> {
+    return new Promise<void>((resolve, reject) => {
+      if (this._fd === undefined) {
+        reject("No open file handle found");
+        return;
+      }
+      fs.close(this._fd, (err) => {
+        if (err) {
+          reject(err);
+          return;
+        }
+        resolve();
+      });
+    });
+  }
+
+  public slice(start: number, end: number): Promise<Uint8Array> {
+    return new Promise<Uint8Array>((resolve, reject) => {
+      if (this._fd === undefined) {
+        reject("No open file handle found");
+        return;
+      }
+      const buffer = Buffer.alloc(end - start);
+      fs.read(
+        this._fd,
+        uint8FromBufLike(buffer),
+        0,
+        end - start,
+        start,
+        (err: unknown) => {
+          if (err) {
+            reject(err);
+            return;
+          }
+          resolve(new Uint8Array(buffer));
+        },
+      );
+    });
+  }
+}
